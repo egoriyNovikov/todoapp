5a916d265662353d36c19386cdbe9c3e246b4690
Egor Novikov <egoriyNovikov@gmail.com>
2026-05-19 18:39:53 +0300

    refacrtoring statistics

--- internal/feathers/statistics/service/service.go
+++ internal/feathers/statistics/service/service.go
@@ -6,13 +6,7 @@ import (
 )
 
 type StatisticsService interface {
-	GetCompletedTasks(userID string) (statistics.CompletedTasksByUser, error)
-	GetTasksInCompleted(userID string) (statistics.TasksInCompletedByUser, error)
-	GetAllTasks() (statistics.AllTasks, error)
-	GetAllTasksByUser(userID string) (statistics.AllTasksByUser, error)
-	GetAllCompletedTasks(userID string) (statistics.AllCompletedTasks, error)
-	GetAllInCompletedTasks(userID string) (statistics.AllInCompletedTasks, error)
-	GetAllUsersStatistics(userID string) (statistics.GetAllUsersStatistics, error)
+	GetSummaryStatistics(userID string) (statistics.SummaryStatistics, error)
 }
 
 type service struct {
@@ -23,30 +17,6 @@ func NewService(r statistics_repository.StatisticsRepository) StatisticsService 
 	return &service{repo: r}
 }
 
-func (s *service) GetCompletedTasks(userID string) (statistics.CompletedTasksByUser, error) {
-	return s.repo.GetCompletedTasks(userID)
-}
-
-func (s *service) GetTasksInCompleted(userID string) (statistics.TasksInCompletedByUser, error) {
-	return s.repo.GetTasksInCompleted(userID)
-}
-
-func (s *service) GetAllTasks() (statistics.AllTasks, error) {
-	return s.repo.GetAllTasks()
-}
-
-func (s *service) GetAllTasksByUser(userID string) (statistics.AllTasksByUser, error) {
-	return s.repo.GetAllTasksByUser(userID)
-}
-
-func (s *service) GetAllCompletedTasks(userID string) (statistics.AllCompletedTasks, error) {
-	return s.repo.GetAllCompletedTasks(userID)
-}
-
-func (s *service) GetAllInCompletedTasks(userID string) (statistics.AllInCompletedTasks, error) {
-	return s.repo.GetAllInCompletedTasks(userID)
-}
-
-func (s *service) GetAllUsersStatistics(userID string) (statistics.GetAllUsersStatistics, error) {
-	return s.repo.GetAllUsersStatistics(userID)
+func (s *service) GetSummaryStatistics(userID string) (statistics.SummaryStatistics, error) {
+	return s.repo.GetSummaryStatistics(userID)
 }
--- internal/feathers/statistics/tranport/http/http.go
+++ internal/feathers/statistics/tranport/http/http.go
@@ -15,71 +15,12 @@ func NewStatisticsHandler(s statistics_service.StatisticsService) *StatisticsHan
 	return &StatisticsHandler{service: s}
 }
 
-func (h *StatisticsHandler) GetCompletedTasks(w http.ResponseWriter, r *http.Request) {
+func (h *StatisticsHandler) GetSummaryStatistics(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("userID")
-	completedTasks, err := h.service.GetCompletedTasks(userID)
+	summaryStatistics, err := h.service.GetSummaryStatistics(userID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	json.NewEncoder(w).Encode(completedTasks)
-}
-
-func (h *StatisticsHandler) GetTasksInCompleted(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("userID")
-	incompletedTasks, err := h.service.GetTasksInCompleted(userID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(incompletedTasks)
-}
-
-func (h *StatisticsHandler) GetAllTasks(w http.ResponseWriter, r *http.Request) {
-	allTasks, err := h.service.GetAllTasks()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(allTasks)
-}
-
-func (h *StatisticsHandler) GetAllTasksByUser(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("userID")
-	allTasks, err := h.service.GetAllTasksByUser(userID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(allTasks)
-}
-
-func (h *StatisticsHandler) GetAllCompletedTasks(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("userID")
-	allCompletedTasks, err := h.service.GetAllCompletedTasks(userID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(allCompletedTasks)
-}
-
-func (h *StatisticsHandler) GetAllInCompletedTasks(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("userID")
-	allInCompletedTasks, err := h.service.GetAllInCompletedTasks(userID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(allInCompletedTasks)
-}
-
-func (h *StatisticsHandler) GetAllUsersStatistics(w http.ResponseWriter, r *http.Request) {
-	userID := r.PathValue("userID")
-	allUsersStatistics, err := h.service.GetAllUsersStatistics(userID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(allUsersStatistics)
+	json.NewEncoder(w).Encode(summaryStatistics)
 }
